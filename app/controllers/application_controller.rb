b282795bd17f168a1deff0e26650b7716381b95d
Mike Miller <mikemille999@gmail.com>
2021-10-14 10:43:19 -0500

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < ActionController::API
+  def actor
+    
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+#Actor.create({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
+Actor.create({first_name: "Tom", last_name: "Holland", known_for: "Spider-man"})
+Actor.create({first_name: "Vin", last_name: "Diesel", known_for: "Chronicles of Riddick"})
+Actor.create({first_name: "Shia", last_name: "Lebouf", known_for: "Transformers"})
