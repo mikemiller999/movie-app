de73befd53ce22b09fb03ccc5b486c5dda1c5751
Mike Miller <mikemille999@gmail.com>
2021-10-24 23:54:51 -0500

    adds has_many hw content

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :actors
   validates :title, presence: true
   validates :plot, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_164056) do
+ActiveRecord::Schema.define(version: 2021_10_25_043942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_10_22_164056) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "age"
     t.string "gender"
+    t.integer "movie_id"
   end
 
   create_table "movies", force: :cascade do |t|
