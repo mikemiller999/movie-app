9e1550ed4cf9632cf01c0854d78ff6ad62fb0ece
Mike Miller <mikemille999@gmail.com>
2021-11-02 02:56:09 -0500

    adds thursdays hw

---
+++ app/models/genre.rb
@@ -0,0 +1,4 @@
+class Genre < ApplicationRecord
+  has_many :movie_genres
+
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :movie_genres
   validates :title, presence: true
   validates :plot, presence: true
 end
