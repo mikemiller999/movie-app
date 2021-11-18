18d5af5cfcca7753aab1a169092dbdaaa95aadd7
Mike Miller <mikemille999@gmail.com>
2021-11-17 23:52:16 -0600

    adds hw

--- public/moviesIndex.js
+++ public/moviesIndex.js
@@ -1,6 +1,8 @@
 /* global axios */
 
-axios.get("http://localhost:3000/actors").then(function (response) {
-  var actors = response.data;
-  console.log(actors, this.actors);
-});
+function indexActors() {
+  axios.get("http://localhost:3000/actors").then(function (response) {
+    var actors = response.data;
+    console.log(actors, this.actors);
+  });
+}
